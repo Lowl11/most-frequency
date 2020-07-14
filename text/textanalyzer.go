f12cfc16954a242a78951adf5bf3062d9864b829
Lazy Owl <owl.lazy@yandex.kz>
2020-07-14 14:39:42 +0600

    Убраны любые символы кроме букв для конкретного поиска

--- text/textanalyzer.go
+++ text/textanalyzer.go
@@ -1,5 +1,11 @@
 package text
 
+import (
+	"fmt"
+	"regexp"
+	"strings"
+)
+
 type TextAnalyzer struct {
 	Words                       []string
 	MostFrequentlyWordsQuantity int
@@ -8,17 +14,28 @@ type TextAnalyzer struct {
 // Main method of analyzer which will find the most frequently words
 func (ta *TextAnalyzer) FindMostFrequentlyWords() {
 	ta.prepareTextWords()
+	for _, word := range ta.Words {
+		fmt.Println(word)
+	}
 }
 
 func (ta *TextAnalyzer) prepareTextWords() {
 	ta.removeEmptyWords()
+	ta.removeSymbols()
+}
+
+func (ta *TextAnalyzer) removeSymbols() {
+	regex, _ := regexp.Compile("[^a-zA-Z]+")
+	for i := 0; i < len(ta.Words); i++ {
+		ta.Words[i] = regex.ReplaceAllString(ta.Words[i], "")
+	}
 }
 
 func (ta *TextAnalyzer) removeEmptyWords() {
 	out := ta.Words[:0]
 	for _, word := range ta.Words {
 		if word != "" {
-			out = append(out, word)
+			out = append(out, strings.TrimSpace(word))
 		}
 	}
 	ta.Words = out
